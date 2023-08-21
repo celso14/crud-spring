a9be87ee17bb5bef296fc5d337693d954cc39629
Bcelso14 <botelhocelso99@gmail.com>
2023-08-21 13:51:53 -0300

    config: cors configuration

---
+++ src/main/java/com/celso/crudspring/config/CorsConfiguration.java
@@ -0,0 +1,16 @@
+package com.celso.crudspring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**") //path pattern da nossa api
+            .allowedOrigins("*")// Origins permitidos
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS",  "HEAD", "TRACE", "CONNECT");
+    }
+}
